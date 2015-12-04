cfb6a56de9e7c3f837ed333b5a16990dadc0fda9
Matt Osborn <matthew.osborn@theguardian.com>
2015-12-04 10:12:13 +0000

    parse airstrikes

---
+++ src/data/parse-airstrikes.js
@@ -0,0 +1,43 @@
+var csv = require('csv');
+var fs = require('fs');
+var path = require('path');
+var es = require('event-stream')
+var CombinedStream = require('combined-stream')
+
+var filepath = file => path.join(__dirname, '../..', file);
+
+var russianAirstrikes = fs.createReadStream(filepath('data-in/russian-airstrikes.csv'));
+var coalitionAirstrikes = fs.createReadStream(filepath('data-in/coalition-airstrikes.csv'));
+var output = fs.createWriteStream(filepath('data-out/airstrikes.json'), {defaultEncoding: 'utf8'})
+
+function processAirstrikes(input, airforce) {
+  var processAirstrike = es.through(
+    function write(record) {
+      var [date, geo] = record['Date Range and Location'].split(', Point: ');
+      if (geo) {
+        var airstrike = {
+          date: date.split(' to ')[0],
+          geo: geo.split(', ').map(n => parseFloat(n)).join(' '),
+          airforce: airforce
+        };
+        this.queue(JSON.stringify(airstrike));
+      }
+    },
+    function end() {
+      this.queue(null)
+    });
+
+  return input
+    .pipe(csv.parse({columns:true}))
+    .pipe(processAirstrike)
+}
+
+var combinedStream = CombinedStream.create();
+combinedStream.append( processAirstrikes(russianAirstrikes, 'Russia') );
+combinedStream.append( processAirstrikes(coalitionAirstrikes, 'Coalition') );
+
+output.write('[\n');
+
+combinedStream
+  .pipe(es.join(',\n')).on('end', () => output.write('\n]'))
+  .pipe(output)
